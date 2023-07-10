a5e6ce179eddb0f41e7d611ff513fd75f9f99ca9
Aydan Ghazani <ghazani.aydan@gmail.com>
2023-07-10 14:46:30 +0000

    More prints

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -24,8 +24,11 @@ class Function:
 
   @classmethod
   def apply(fxn:Type[Function], *x:Tensor, **kwargs) -> Tensor:
+    print("Apply: fxn", fxn)
     ctx = fxn(x[0].device, *x)
+    print("Apply: ctx", ctx)
     ret = Tensor(ctx.forward(*[t.lazydata for t in x], **kwargs), device=ctx.device, requires_grad=ctx.requires_grad)
+    print("Apply: ret", ret)
     if ctx.requires_grad and not Tensor.no_grad: ret._ctx = ctx    # used by autograd engine
     return ret
 
